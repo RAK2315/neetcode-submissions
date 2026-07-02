667d45b093517d276418d874fb7bcf407eb03244
Rehaan <rehtrooper@gmail.com>
2026-07-02 09:10:10 +0530

    Add: daily-temperatures - submission-4

---
+++ Data Structures & Algorithms/daily-temperatures/submission-4.py
@@ -0,0 +1,51 @@
+class Solution:
+    def dailyTemperatures(self, temps: List[int]) -> List[int]:
+        
+        """ 
+        MY SOLUTION before stack
+        
+        final=[]
+        for i in range(len(temps)):
+            j=i
+            count=0
+            foundLarger=False
+            while j<len(temps):
+                if temps[j]>temps[i]:
+                    foundLarger=True
+                    break
+                j+=1
+                count+=1
+            
+            if foundLarger: final.append(count)
+            else: final.append(0)
+        return final
+
+        worst case: o(n2)
+        """
+
+        final=[0]*len(temps)
+        stack=[]
+        for index, temp in enumerate(temps):
+            while stack and temp > stack[-1][0]: # While current temp is greater than the last stacked temp
+                stackTemp, stackIndex = stack.pop()
+                final[stackIndex] = index - stackIndex   # Distance to warmer day
+            stack.append([temp, index])  #Push current temp and index onto stack
+        return final
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
