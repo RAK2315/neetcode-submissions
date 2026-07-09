b4f9ae36ff5eee55298b8e6f8e844ff0a9e985e0
Rehaan <rehtrooper@gmail.com>
2026-07-09 22:13:47 +0530

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,43 @@
+class TimeMap:
+
+    def __init__(self):
+        self.di = {} # form: {key:[(timestamp,value), (timestamp2,value2)]}
+
+        # also given timestampt 1 will be less then t2 always and so on t1<t2<t3...
+        # so hint binary search
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.di:
+            self.di[key]=[ (timestamp,value) ]
+        else:
+            self.di[key].append((timestamp,value))
+
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.di: return ""
+
+        arr = self.di[key] #[ (t1,v1), (t2,v2) ]
+        left ,right = 0, len(arr) - 1
+        target = timestamp
+
+        while left <= right:
+            mid = (left+right)//2
+
+            if arr[mid][0] == target:
+                return arr[mid][1]
+            
+            elif arr[mid][0] < target:
+                left = mid + 1
+            else: right = mid - 1
+        
+        if right == -1: return "" 
+        # ex: [(3,a),(4,b),(5,c)], target tstamp = 2
+        # so right would keep decreasing till final loop makes it -1
+
+        else: return arr[right][1]
+
+
+
+
+        
