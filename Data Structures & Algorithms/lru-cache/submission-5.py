7b1482cfd0963a7f59ce1f818b0b41b70a100d98
Rehaan <rehtrooper@gmail.com>
2026-08-01 21:13:11 +0530

    Add: lru-cache - submission-5

---
+++ Data Structures & Algorithms/lru-cache/submission-5.py
@@ -0,0 +1,37 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            # delete and add the key again so that it becomes them most recent key
+            value = self.cache[key]
+            del self.cache[key]
+            self.cache[key] =  value
+
+            return self.cache[key]
+        return -1
+
+        
+
+    def put(self, key: int, value: int) -> None:
+        # 1. if key exist, update value, delete and add again to make it most recent used
+        if key in self.cache:
+            del self.cache[key]
+
+            self.cache[key] = value
+
+
+        else:
+            # 2. if key does not exist, remove least recently used key, ie
+            # the key not used for longest time, which would be the first key
+            if len(self.cache) == self.capacity:
+                first_key = next(iter(self.cache)) # o(1)
+                del self.cache[first_key]
+            
+            # add the key value pair
+            self.cache[key] = value
+        
+        
